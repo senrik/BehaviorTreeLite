cfe4274276e87776fdf3e71bce1ca40b88937197
senrik <13647220+senrik@users.noreply.github.com>
2025-08-29 14:31:27 -0400

    Added placeholders for WanderTask and PatrolTask

--- BehaviorTreeTask.cs
+++ BehaviorTreeTask.cs
@@ -6,5 +6,38 @@ namespace BehaviorTree
         public string label;
         public abstract BehaviorTreeState PerformTask(IBehaviorTreeAgent instance);
     }
+
+    [System.Serializable]
+    public class WanderTask : BehaviorTreeTask
+    {
+        public override BehaviorTreeState PerformTask(IBehaviorTreeAgent instance)
+        {
+            if(instance.MovementType == MovementType.Wander)
+            {
+                return BehaviorTreeState.Running;
+            }
+            else
+            {
+                return BehaviorTreeState.Failure;
+            }
+                
+        }
+    }
+
+    [System.Serializable]
+    public class PatrolTask : BehaviorTreeTask
+    {
+        public override BehaviorTreeState PerformTask(IBehaviorTreeAgent instance)
+        {
+            if (instance.MovementType == MovementType.Patrol)
+            {
+                return BehaviorTreeState.Running;
+            }
+            else
+            {
+                return BehaviorTreeState.Failure;
+            }
+        }
+    }
 }
 
