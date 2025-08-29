0b1d7061a1745a7d646cccab62440e399f8ba222
senrik <13647220+senrik@users.noreply.github.com>
2025-08-29 14:29:35 -0400

    Added copy constructor header
    Filled out IBehaviorTreeAgent with list of properties

--- BehaviorTree.cs
+++ BehaviorTree.cs
@@ -1,10 +1,28 @@
+using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.AI;
+
 
 namespace BehaviorTree
 {
+    [System.Serializable]
     public class BehaviorTree
     {
+        [SerializeReference]
         BehaviorTreeNode root;
+        [SerializeField]
+        List<BehaviorTreeNode> nodes;
+
+        public BehaviorTree()
+        {
+            nodes = new List<BehaviorTreeNode>();
+        }
+        public BehaviorTree(BehaviorTree other)
+        {
+            // TODO: Implement deep copy
+        }
+
         public void EvaluateTree(IBehaviorTreeAgent instance)
         {
             root.EvaluateNode(instance);
@@ -26,9 +44,81 @@ namespace BehaviorTree
 
     public interface IBehaviorTreeAgent
     {
+        public MovementType MoveType
+        {
+            get; set;
+        }
 
+        public List<Vector3> PatrolPath
+        {
+            get; set;
+        }
+
+        public int CurrentPatrolPoint
+        {
+            get; set;
+        }
+
+        public float PatrolThreshold
+        {
+            get;
+        }
+
+        public Transform Target
+        {
+            get;
+
+        }
+        public NavMeshAgent Agent
+        {
+            get;
+        }
+
+        public Animator CharAnim
+        {
+            get;
+        }
+
+        public Vector3 TetherPoint
+        {
+            get;
+        }
+
+        public Transform Transform
+        {
+            get;
+        }
+
+        public bool InAttackRange
+        {
+            get;
+            set;
+        }
+
+        public MovementType MovementType { get; }
+
+        public float WanderWaitTimer
+        {
+            get; set;
+        }
+
+        public Vector3 WanderPoint
+        {
+            get; set;
+        }
+
+        public float WanderRange
+        {
+            get; set;
+        }
+
+        public BehaviorStats Stats
+        {
+            get;
+        }
     }
 
+    [System.Serializable]
     public class BehaviorStats
     {
         public float attackRange;
