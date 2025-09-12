66f5faa7ab5a4ba3c84d7f4264ad5623a07e5685
senrik <13647220+senrik@users.noreply.github.com>
2025-09-12 10:17:22 -0400

    Added stand in tasks for patrol and wander

--- BehaviorTreeTask.cs
+++ BehaviorTreeTask.cs
@@ -4,12 +4,22 @@ namespace BehaviorTree
     public abstract class BehaviorTreeTask
     {
         public string label;
+
+        public abstract BehaviorTreeTask Clone();
         public abstract BehaviorTreeState PerformTask(IBehaviorTreeAgent instance);
     }
 
     [System.Serializable]
     public class WanderTask : BehaviorTreeTask
     {
+        public WanderTask()
+        {
+            label = "Wander";
+        }
+        public override BehaviorTreeTask Clone()
+        {
+            return new WanderTask();
+        }
         public override BehaviorTreeState PerformTask(IBehaviorTreeAgent instance)
         {
             if(instance.MovementType == MovementType.Wander)
@@ -27,6 +37,15 @@ namespace BehaviorTree
     [System.Serializable]
     public class PatrolTask : BehaviorTreeTask
     {
+        public PatrolTask()
+        {
+            label = "Patrol";
+        }
+
+        public override BehaviorTreeTask Clone()
+        {
+            return new PatrolTask();
+        }
         public override BehaviorTreeState PerformTask(IBehaviorTreeAgent instance)
         {
             if (instance.MovementType == MovementType.Patrol)
