e8c65483fc9d7e42046ef9fb7fde222740647503
senrik <13647220+senrik@users.noreply.github.com>
2025-09-16 16:04:00 -0400

    Added edge drawing to the BehaviorTreeEditorWindow.

--- Editor/BehaviorTreeEditorWindow.cs
+++ Editor/BehaviorTreeEditorWindow.cs
@@ -16,6 +16,11 @@ public class BehaviorTreeEditorWindow : EditorWindow
     NodeDisplay selectedNode;
     bool selectingNode = false;
     List<NodeDisplay> nodes;
+    
+    HashSet<NodeEdge> edges;
+    NodeEdge openEdge;
+    bool makingConnection = false;
+    Vector2 pointerPosition;
     BehaviorTreeTasks tasks;
 
     [MenuItem("Behavior Trees/Tree Builder")]
@@ -34,6 +39,7 @@ public class BehaviorTreeEditorWindow : EditorWindow
         executorNodeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(btEditorAssetPath + "Behavior Trees/Editor/BehaviorTreeExecutorNode_UXML.uxml");
 
         nodes = new List<NodeDisplay>();
+        edges = new HashSet<NodeEdge>();
         selectedNode = new NodeDisplay();
         tasks = BehaviorTreeTasks.Instance;
     }
@@ -60,7 +66,10 @@ public class BehaviorTreeEditorWindow : EditorWindow
                 foreach(var node in nodes)
                 {
                     rootVisualElement.Q<ScrollView>("TreeView").Add(node.nodeElem);
+                    rootVisualElement.Q<ScrollView>("TreeView").MarkDirtyRepaint();
+
                 }
+                
             }
         });
         rootVisualElement.Q<Button>("AddSelectorNodeButton").clicked += () =>
@@ -68,6 +77,23 @@ public class BehaviorTreeEditorWindow : EditorWindow
             var selector = new NodeDisplay();
             selector.node = new BehaviorTreeSelectorNode();
             selector.nodeElem = DrawNode(selector.node, 0);
+            selector.nodeElem.Q<VisualElement>("OutConnections").RegisterCallback<PointerDownEvent>((callback) =>
+            {
+                Debug.Log("Start Connection...");
+                openEdge.start = selector;
+                makingConnection = true;
+            });
+            selector.nodeElem.Q<VisualElement>("InConnections").RegisterCallback<PointerDownEvent>((callback) =>
+            {
+                if(openEdge.start.nodeElem != null)
+                {
+                    Debug.Log("End Connection");
+                    openEdge.end = selector;
+                    edges.Add(openEdge);
+                    openEdge = new NodeEdge();
+                    rootVisualElement.MarkDirtyRepaint();
+                }
+            });
             rootVisualElement.Q<ScrollView>("TreeView").Add(selector.nodeElem);
             nodes.Add(selector);
         };
@@ -76,6 +102,23 @@ public class BehaviorTreeEditorWindow : EditorWindow
             var sequencer = new NodeDisplay();
             sequencer.node = new BehaviorTreeSequencerNode();
             sequencer.nodeElem = DrawNode(sequencer.node, 0);
+            sequencer.nodeElem.Q<VisualElement>("OutConnections").RegisterCallback<PointerDownEvent>((callback) =>
+            {
+                Debug.Log("Start Connection...");
+                openEdge.start = sequencer;
+                makingConnection = true;
+            });
+            sequencer.nodeElem.Q<VisualElement>("InConnections").RegisterCallback<PointerDownEvent>((callback) =>
+            {
+                if (openEdge.start.nodeElem != null)
+                {
+                    Debug.Log("End Connection");
+                    openEdge.end = sequencer;
+                    edges.Add(openEdge);
+                    openEdge = new NodeEdge();
+                    rootVisualElement.MarkDirtyRepaint();
+                }
+            });
             rootVisualElement.Q<ScrollView>("TreeView").Add(sequencer.nodeElem);
             nodes.Add(sequencer);
         };
@@ -84,6 +127,23 @@ public class BehaviorTreeEditorWindow : EditorWindow
             var executor = new NodeDisplay();
             executor.node = new BehaviorTreeExecutorNode();
             executor.nodeElem = DrawNode(executor.node, 0);
+            executor.nodeElem.Q<VisualElement>("OutConnections").RegisterCallback<PointerDownEvent>((callback) =>
+            {
+                Debug.Log("Start Connection...");
+                openEdge.start = executor;
+                makingConnection = true;
+            });
+            executor.nodeElem.Q<VisualElement>("InConnections").RegisterCallback<PointerDownEvent>((callback) =>
+            {
+                if (openEdge.start.nodeElem != null)
+                {
+                    Debug.Log("End Connection");
+                    openEdge.end = executor;
+                    edges.Add(openEdge);
+                    openEdge = new NodeEdge();
+                    rootVisualElement.MarkDirtyRepaint();
+                }
+            });
             rootVisualElement.Q<ScrollView>("TreeView").Add(executor.nodeElem);
             nodes.Add(executor);
         };
@@ -110,8 +170,46 @@ public class BehaviorTreeEditorWindow : EditorWindow
                 }
                 
             }
+
+            if(openEdge.start.nodeElem != null)
+            {
+                if (makingConnection)
+                {
+                    makingConnection = false;
+                }else
+                {
+                    
+                    if(openEdge.end.nodeElem == null)
+                    {
+                        Debug.Log("Clearing Connection");
+                        openEdge.start = new NodeDisplay();
+                    }
+                        
+                }
+            }
+
         });
 
+        rootVisualElement.generateVisualContent += DrawEdges;
+
+    }
+
+    void DrawEdges(MeshGenerationContext ctx)
+    {
+        var painter = ctx.painter2D;
+        painter.lineWidth = 10;
+        painter.strokeColor = Color.white;
+        foreach(var edge in edges)
+        {
+            painter.BeginPath();
+            var lineStart = edge.start.nodeElem.Q<VisualElement>("OutConnections").worldBound.center;
+            lineStart.y -= edge.start.nodeElem.Q<VisualElement>("OutConnections").worldBound.height;
+            var lineEnd = edge.end.nodeElem.Q<VisualElement>("InConnections").worldBound.center;
+            painter.MoveTo(lineStart);
+            painter.LineTo(lineEnd);
+            painter.Stroke();
+            
+        }
     }
 
     private void ParseTree(BehaviorTree.BehaviorTree t)
@@ -127,13 +225,14 @@ public class BehaviorTreeEditorWindow : EditorWindow
             display.nodeElem = DrawNode(current, queue.Count);
             nodes.Add(display);
             queue.RemoveAt(0);
+            
 
-            var child = current.firstChildNode;
+            var child = current.FirstChildNode;
             while(child != null)
             {
-                queue.Add((BehaviorTreeNode)child);
+                queue.Add(child);
 
-                child = child.siblingNode;
+                child = child.SiblingNode;
             }
         }
     }
@@ -181,15 +280,39 @@ public class BehaviorTreeEditorWindow : EditorWindow
                 root.Q<VisualElement>("NodeBodyElem").AddToClassList("selected-node");
                 
             }
-            
+            rootVisualElement.MarkDirtyRepaint();
         });
+        
 
         return root;
+    }
+
+    private void Update()
+    {
+        Debug.Log($"Edges Count: {edges.Count}");
     }
 
     struct NodeDisplay
     {
         public BehaviorTreeNode node;
         public VisualElement nodeElem;
+    }
+
+    struct NodeEdge : IComparable<NodeEdge> {
+        public NodeDisplay start;
+        public NodeDisplay end;
+
+        public readonly int CompareTo(NodeEdge other)
+        {
+            if(this.start.nodeElem == other.start.nodeElem && this.end.nodeElem == other.end.nodeElem)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
     }
 }
